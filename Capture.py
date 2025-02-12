10c7e8cd28eb9a6354bf4a9bb47f8afbff30b632
Duba Bence <duba.bence22@gmail.com>
2025-02-12 15:36:11 +0100

    Lines

--- Capture.py
+++ Capture.py
@@ -1,6 +1,6 @@
 import cv2 
 import numpy as np
-from LineDetection import cannyImage, region_triangle,bluredImage,ModifyImages
+from LineDetection import cannyImage, region_triangle,bluredImage, display_lines
 import matplotlib.pyplot as plt
 
 image = cv2.VideoCapture("example.mp4")
@@ -15,8 +15,13 @@ paused = False
 while image.isOpened():
     if not paused:
         ret, frame = image.read()
-        all_res = ModifyImages(frame)
-        cv2.imshow('frame',all_res)
+        frame_img = np.copy(frame)
+        canny = cannyImage(frame_img)
+        cropped = region_triangle(canny)
+        lines = cv2.HoughLinesP(cropped, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+        line_frame =display_lines(frame, lines)
+        combo_image = cv2.addWeighted(frame, 0.8, line_frame, 1, 1) 
+        cv2.imshow('frame',cropped)
 
     key = cv2.waitKey(int(frame_delay * 200)) & 0xFF
     if key == ord('q'):
--- LineDetection.py
+++ LineDetection.py
@@ -1,14 +1,15 @@
 import cv2 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def cannyImage(frame):
-    grayImage = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     bluredImage = cv2.GaussianBlur(grayImage, (5,5), 0)
     cannyImage = cv2.Canny(bluredImage, 80,100)
     return cannyImage
 
 def bluredImage(frame):
-    grayImage = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     bluredImage = cv2.GaussianBlur(grayImage, (5,5), 0)
     return bluredImage
 
@@ -16,16 +17,15 @@ def region_triangle(frame):
     height = frame.shape[0]
     polygons = np.array([[(50, height), (1150, height), (750, 350)]]) #array x and y and the top endpoint
     mask = np.zeros_like(frame)
-    cv2.fillPoly(mask, polygons, 200)
-    masked_image = cv2.bitwise_and(frame, mask)
+    cv2.fillPoly(mask, polygons, (255,255,255))
+    masked_image = cv2.bitwise_and(frame, mask)    
     return masked_image
 
-def ModifyImages(frame):
-    canny = cannyImage(frame)
-    blured = bluredImage(frame)
-    maskedRoad =  region_triangle(bluredImage(frame))
-    masked =  region_triangle(canny)
-    row1 = np.hstack((blured, canny))
-    row2 = np.hstack((masked, maskedRoad))
-    combined  = np.vstack((row1, row2))
-    return combined
+
+def display_lines(frame, lines):
+    line_image = np.zeros_like(frame)   
+    if lines is not None:
+        for line in lines:
+            x1,y1,x2,y2 = line.reshape(4)   
+            line = cv2.line(line_image, (x1,y1), (x2,y2), (255,0,0), 10)
+    return line_image
