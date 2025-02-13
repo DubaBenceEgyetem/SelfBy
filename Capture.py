74699845d55e62dc42d22c5a6fbb6a18bc3338bb
Bence <bence22@gmail.com>
2025-02-13 14:15:28 +0100

    refactor

--- Capture.py
+++ Capture.py
@@ -1,9 +1,9 @@
 import cv2 
 import numpy as np
-from LineDetection import cannyImage, region_triangle,bluredImage, display_lines
+from LineDetection import processImage, region_triangle, display_lines, avg_slope_intersept, coordinates
 import matplotlib.pyplot as plt
 
-image = cv2.VideoCapture("example.mp4")
+image = cv2.VideoCapture("example2.mp4")
 cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("frame", 1500,700)
 fps = image.get(cv2.CAP_PROP_FPS)
@@ -15,13 +15,14 @@ paused = False
 while image.isOpened():
     if not paused:
         ret, frame = image.read()
-        frame_img = np.copy(frame)
-        canny = cannyImage(frame_img)
+        canny = processImage(frame)
         cropped = region_triangle(canny)
-        lines = cv2.HoughLinesP(cropped, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-        line_frame =display_lines(frame, lines)
+        lines = cv2.HoughLinesP(cropped, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=10)
+        avg_lines = avg_slope_intersept(frame, lines)
+        line_frame =display_lines(frame, avg_lines)
         combo_image = cv2.addWeighted(frame, 0.8, line_frame, 1, 1) 
-        cv2.imshow('frame',cropped)
+        cv2.imshow('frame',combo_image)
+        cv2.waitKey(1)
 
     key = cv2.waitKey(int(frame_delay * 200)) & 0xFF
     if key == ord('q'):
--- LineDetection.py
+++ LineDetection.py
@@ -2,22 +2,17 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def cannyImage(frame):
+def processImage(frame):
     grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     bluredImage = cv2.GaussianBlur(grayImage, (5,5), 0)
     cannyImage = cv2.Canny(bluredImage, 80,100)
     return cannyImage
 
-def bluredImage(frame):
-    grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    bluredImage = cv2.GaussianBlur(grayImage, (5,5), 0)
-    return bluredImage
-
 def region_triangle(frame):
     height = frame.shape[0]
     polygons = np.array([[(50, height), (1150, height), (750, 350)]]) #array x and y and the top endpoint
     mask = np.zeros_like(frame)
-    cv2.fillPoly(mask, polygons, (255,255,255))
+    cv2.fillPoly(mask, polygons, 200)
     masked_image = cv2.bitwise_and(frame, mask)    
     return masked_image
 
@@ -25,7 +20,51 @@ def region_triangle(frame):
 def display_lines(frame, lines):
     line_image = np.zeros_like(frame)   
     if lines is not None:
+        print(lines, type(lines))
         for line in lines:
-            x1,y1,x2,y2 = line.reshape(4)   
-            line = cv2.line(line_image, (x1,y1), (x2,y2), (255,0,0), 10)
+             if line is not None and isinstance(line, np.ndarray) and line.shape == (4,):  
+                x1, y1, x2, y2 = map(int, line.tolist())  # Convert NumPy array to Python integers
+                cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
     return line_image
+
+def avg_slope_intersept(frame, lines):
+    left_fit = []    
+    right_fit = []
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            parameters = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = parameters[0]
+            intercept = parameters[1]
+            if slope < 0:
+                left_fit.append((slope, intercept))
+            else:
+                right_fit.append((slope, intercept))
+        if left_fit:
+            left_fit_avg = np.mean(left_fit, axis=0) 
+            print(left_fit_avg, 'left')
+            left_line = coordinates(frame, left_fit_avg)
+        else:
+            left_line = None
+        if right_fit:
+            right_fit_avg = np.mean(right_fit, axis=0) 
+            print(right_fit_avg, 'right')
+            right_line = coordinates(frame, right_fit_avg)
+        else:
+            right_line = None
+    else:
+        left_line = None
+        right_line = None
+    return [left_line, right_line]
+
+def coordinates(frame, line_parameters):
+    try:
+        slope, intercept = line_parameters
+    except TypeError:
+        slope, intercept = 0.001, 0
+    y1 = frame.shape[0]
+    y2 = int(y1 * (3/5))
+    x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
+
+    return np.array([x1,y1,x2,y2])
